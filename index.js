4e031504be4d8e7f6b0233c34eb33774cc4887e1
dorothy <dorothy.amondi@student.moringaschool.com>
2022-08-22 13:58:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(myTask ='roller-skate'){
+
+    return (`This Saturday, I want to ${myTask}!`);
+
+}
+saturdayFun('bathe my dog');
+
+const mondayWork=function(activity ='go to the office'){
+
+return `This Monday, I will ${activity}.`
+}
+mondayWork('attend a meeting')
+
+
+function wrapAdjective(style = "||") {
+  return function (innerParam = "special") {
+    return `You are ${style}${innerParam}${style}!`;
+  }}
